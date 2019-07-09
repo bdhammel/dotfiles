7e4aaf9ae6766718f703dd67b96772196f8de007
Ben Hammel <bdhammel@gmail.com>
2019-07-09 14:19:12 -0700

    Add custom pudb stringigier

--- ipython/profile_default/startup/ipython_startup.py
+++ ipython/profile_default/startup/ipython_startup.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pprint import pprint
 plt.ion()
 
 print("""
      import numpy as np
      import matplotlib.pyplot as plt
+     from pprint import pprint
      plt.ion()
 
      """)
