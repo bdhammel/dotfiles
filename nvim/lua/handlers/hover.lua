0047be744e73bc4cff8ac15368b0d2c8d501c8fa
Ben Hammel <bdhammel@gmail.com>
2024-08-06 08:30:42 -0700

    Try treesitter

--- nvim/lua/handlers/hover.lua
+++ nvim/lua/handlers/hover.lua
@@ -1,32 +1,10 @@
 local M = {}
 
--- Define a mapping for HTML entities
-local html_entities = {
-    ["&nbsp;"] = " ",
-    ["&lt;"] = "<",
-    ["&gt;"] = ">",
-    ["&amp;"] = "&",
-    ["&quot;"] = "\"",
-    ["&apos;"] = "'",
-}
-
--- Function to decode HTML entities
-local function decode_html_entities(str)
-    if type(str) ~= "string" then
-        return str
-    end
-    return (str:gsub("(&[%w%#]+;)", function(entity)
-        return html_entities[entity] or entity
-    end))
-end
-
 M.setup = function()
   vim.lsp.handlers["textDocument/hover"] = function(err, result, ctx, config)
+    config = config or {}  -- Ensure config is not nil
     local api = vim.api
-    local util = vim.lsp.util
 
-    config = config or {}
-    config.focus_id = ctx.method
     if api.nvim_get_current_buf() ~= ctx.bufnr then
       -- Ignore result since buffer changed. This happens for slow language servers.
       return
@@ -39,18 +17,15 @@ M.setup = function()
       return
     end
 
+    local format = 'markdown'
     local contents ---@type string[]
-    -- local format = 'markdown'
-    -- if type(result.contents) == 'table' and result.contents.kind == 'plaintext' then
-    --   format = 'plaintext'
-    --   contents = vim.split(result.contents.value or '', '\n', { trimempty = true })
-    -- else
-    --   contents = util.convert_input_to_markdown_lines(result.contents)
-    -- end
-    -- contents = decode_html_entities(contents)
-
-    format = 'plaintext'
-    contents = vim.split(result.contents.value or '', '\n', { trimempty = true })
+    if type(result.contents) == 'table' and result.contents.kind == 'plaintext' then
+      format = 'plaintext'
+      contents = vim.split(result.contents.value or '', '\n', { trimempty = true })
+    else
+      contents = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+      contents = vim.lsp.util.trim_empty_lines(contents)
+    end
 
     if vim.tbl_isempty(contents) then
       if config.silent ~= true then
@@ -59,14 +34,30 @@ M.setup = function()
       return
     end
 
-    -- Open a new vertical split and set the contents
-    vim.cmd('vsplit')
+    -- Open a new split window
+    vim.cmd('split')
+
     local buf = api.nvim_create_buf(false, true)
-    api.nvim_buf_set_lines(buf, 0, -1, false, contents)
+
+    -- Set up the contents, using treesitter for markdown
+    local do_stylize = format == 'markdown' and vim.g.syntax_on ~= nil
+    if do_stylize then
+      contents = vim.split(table.concat(contents, '\n'):gsub('\r', ''):gsum('&nbsp;', ' '), '\n', { trimempty = true })
+
+      vim.bo[buf].filetype = 'markdown'
+      api.nvim_buf_set_lines(buf, 0, -1, false, contents)
+      vim.treesitter.start(buf)
+    else
+      -- Clean up input: trim empty lines
+      contents = vim.split(table.concat(contents, '\n'), '\n', { trimempty = true })
+
+      if format then
+        vim.bo[buf].syntax = format
+      end
+      api.nvim_buf_set_lines(buf, 0, -1, true, contents)
+    end
     api.nvim_win_set_buf(0, buf)
-    api.nvim_buf_set_option(buf, 'modifiable', false)
-
   end
-
 end
+
 return M
