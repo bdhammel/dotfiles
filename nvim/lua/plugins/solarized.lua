b4391e5b357240515a93d8279195e55f5563ca50
Ben Hammel <bdhammel@gmail.com>
2024-10-02 09:22:55 -0700

    Pin solarized version

--- nvim/lua/plugins/solarized.lua
+++ nvim/lua/plugins/solarized.lua
@@ -1,6 +1,7 @@
 return {
   'maxmx03/solarized.nvim',
   lazy = false,
+  tag = 'v2.3.0',
   priority = 1000,
   config = function()
     require('solarized').setup({
@@ -22,3 +23,24 @@ return {
 
   end
 }
+-- return {
+--   'maxmx03/solarized.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.o.termguicolors = true
+--     vim.o.background = 'dark'
+--     require('solarized').setup({
+--       on_colors = function(colors, color)
+--         return {
+--           base03 = '#000000', -- background tone dark (main)
+--           base02 = '#2e2e2e', -- background tone (highlight/menu/LineNr)
+--         }
+--       end,
+--     })
+--     vim.cmd.colorscheme 'solarized'
+--     vim.g.lightline = {
+--       colorscheme = 'selenized_black'
+--     }
+--   end,
+-- }
